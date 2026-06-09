275e23e9ad79bb68f77191089bfa1f6341a47de2
PutriAmanda16 <putriamandakhairunnisa16@gmail.com>
2026-06-09 08:21:53 +0700

    Fix variable koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,18 +1,18 @@
 <?php
 error_reporting(0);
 ini_set('display_errors', 0);
-// Mengambil variabel yang sudah kita set di tab 'Variables' Railway
+
 $host     = getenv('DB_HOST'); 
 $user     = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $dbname   = getenv('DB_NAME');
 $port     = getenv('DB_PORT');
 
-// Mengkoneksikan ke MySQL memakai Port dinamis
 $conn = new mysqli($host, $user, $password, $dbname, $port);
 
 if ($conn->connect_error) {
-    // Jangan biarkan script mati total tanpa pesan, cek errornya
     die("Koneksi gagal: " . $conn->connect_error);
 }
+
+$koneksi = $conn; // Tambahkan baris ini
 ?>
