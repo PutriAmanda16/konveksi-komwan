13adcfff86d975cc6787e631bba4a381c468027b
PutriAmanda16 <putriamandakhairunnisa16@gmail.com>
2026-06-17 23:31:57 +0700

    Fix

--- owner/dashboard.php
+++ owner/dashboard.php
@@ -15,8 +15,7 @@ $total_produk    = mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM produk"
 $omset              = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT SUM(TOTAL_HARGA) as t FROM pesanan WHERE STATUS='Selesai'"))['t'] ?? 0;
 // Tanpa filter STATUS_TERIMA
 $biaya_gaji = mysqli_fetch_assoc(mysqli_query($koneksi, 
-    "SELECT SUM(TOTAL_UPAH) as t FROM penggajian 
-     WHERE STATUS_GAJI = 'Sudah Dibayar'"))['t'] ?? 0;
+    "SELECT SUM(TOTAL_UPAH) as t FROM penggajian"))['t'] ?? 0;
 // atau coba TOTAL_UPAH kalau JUMLAH_GAJI tetap 0:
 // "SELECT SUM(TOTAL_UPAH) as t FROM penggajian"
 $biaya_bahan        = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT SUM(TOTAL_BIAYA) as t FROM pembelian_bahan"))['t'] ?? 0;
@@ -86,7 +85,7 @@ while ($r = mysqli_fetch_assoc($q_top)) {
 $penjahit_prod = [];
 $q_pj = mysqli_query($koneksi,
     "SELECT pj.NAMA_PENJAHIT, COUNT(pg.ID_GAJI) as total_kerja,
-            SUM(pg.JUMLAH_GAJI) as total_upah, AVG(pg.JUMLAH_GAJI) as avg_upah
+            SUM(pg.TOTAL_UPAH) as total_upah, AVG(pg.TOTAL_UPAH) as avg_upah
      FROM penggajian pg
      JOIN produksi pr ON pg.ID_PRODUKSI = pr.ID_PRODUKSI
      JOIN penjahit pj ON pr.ID_PENJAHIT = pj.ID_PENJAHIT
